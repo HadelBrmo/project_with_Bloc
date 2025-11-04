2823cea8dccf034f7b56e9a25cc911c100c0f30c
Hadel Brmo <brmohadel56@gmail.com>
2025-11-04 23:59:22 +0300

    Handle data and business logical Layers

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:learn_bloc/data/web_services/apiServices.dart';
 import 'package:learn_bloc/presention/screens/characterDetails.dart';
 import 'package:flutter/material.dart';
 import 'package:learn_bloc/presention/screens/charactersScreen.dart';
+import 'bussiess_logic/cubit/character_cubit.dart';
 import 'constant/string.dart';
+import 'data/repositry/character_repo.dart';
 class AppRouter{
+  late final CharactersCubit charactersCubit;
+  late final CharacterRepository characterRepository;
 
+  AppRouter(){
+    characterRepository=CharacterRepository(ApiServices());
+    charactersCubit=CharactersCubit(characterRepository);
+  }
   Route? generateRoute(RouteSettings setting){
     switch(setting.name){
       case characterScreen:
-        return MaterialPageRoute(builder: (_)=>CharactersScreen());
+        return MaterialPageRoute(builder: (_)=>BlocProvider(create:
+        (BuildContext context)=>charactersCubit,
+          child: CharactersScreen(),
+        ),
+        );
         break;
       case charactersDetails:
         return MaterialPageRoute(builder: (_)=>Characterdetails());
